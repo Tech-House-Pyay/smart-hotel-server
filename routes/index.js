69146ca426dd477b5e9a5977c40a8ff88c2fdb6b
maythuthuaung <aungthumaythu8@gmail>
2020-07-04 13:42:17 +0630

    add home route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/home", (req, res) => {
+  res.render("home");
+});
+
 module.exports = router;
