49dbec11c4a7730324f0820f19c2d91c7d090bc8
maythuthuaung <aungthumaythu8@gmail>
2020-07-11 15:27:04 +0630

    add database and room route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var IOset = require("../IOset");
+var clientList = require("../clientList");
+var Room = require("../model/Room");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +11,51 @@ router.get('/', function(req, res, next) {
 
 router.get("/home", (req, res) => {
   res.render("home");
+});
+
+router.get("/roomlist", (req, res) => {
+  var ipList = clientList.getList().map((n) => {
+    return n.ip;
+  });
+  console.log(ipList);
+  Room.find({ ip: { $in: ipList } }, (err, rtn) => {
+    if (err) throw err;
+    console.log(rtn);
+    res.render("room-list", { rooms: rtn });
+  });
+});
+
+router.get("/roomdetail/:no", (req, res) => {
+  Room.findOne({ number: req.params.no }, (err, rtn) => {
+    if (err) throw err;
+    res.render("room-detail", { room: rtn });
+  });
+});
+
+router.get("/addroom", (req, res) => {
+  res.render("room-add");
+});
+
+router.post("/addroom", (req, res) => {
+  var room = new Room();
+  room.number = req.body.rNo;
+  room.ip = req.body.ipAddr;
+  room.password = req.body.password;
+
+  room.save((err, rtn) => {
+    if (err) throw err;
+    res.redirect("/roomlist");
+  });
+});
+
+router.post("/checkroom", (req, res) => {
+  Room.findOne(
+    { $or: [{ number: req.body.number }, { ip: req.body.ip }] },
+    (err, rtn) => {
+      if (err) throw err;
+      rtn != null ? res.json({ status: true }) : res.json({ status: false });
+    }
+  );
 });
 
 module.exports = router;
